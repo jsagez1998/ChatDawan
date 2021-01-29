906c8f4f556b9950fcafda49f62a422451b663ef
Jason <51094681+jsagez1998@users.noreply.github.com>
2021-01-29 17:02:57 +0100

    Add files via upload

--- SpringChat/springchat/src/main/java/org/dawan/springchat/repositories/ChannelRepository.java
+++ SpringChat/springchat/src/main/java/org/dawan/springchat/repositories/ChannelRepository.java
@@ -1,10 +1,20 @@
 package org.dawan.springchat.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 import org.dawan.springchat.entities.Channel;
 
 @Repository
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
 
+	@Query(value = "FROM channel c WHERE c.name = :name")
+    List<Channel> findChannelByName(@Param("name") String name);
+	
+	@Query(value = "FROM channel c WHERE c.id = :id")
+    Channel findAllById(@Param("id") long id);
 }
