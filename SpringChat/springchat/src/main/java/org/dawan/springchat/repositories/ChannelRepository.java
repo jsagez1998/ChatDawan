6b114903dca1b81298b524b9262fc1d883f14540
kyodex <dinehs96@hotmail.fr>
2021-02-11 15:28:15 +0100

    rectification des Repo

--- SpringChat/springchat/src/main/java/org/dawan/springchat/repositories/ChannelRepository.java
+++ SpringChat/springchat/src/main/java/org/dawan/springchat/repositories/ChannelRepository.java
@@ -12,12 +12,12 @@ import org.dawan.springchat.entities.Channel;
 @Repository
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
 
-	@Query(value = "FROM channel c WHERE c.name = :name")
+	@Query(value = "FROM Channel c WHERE c.name = :name")
     List<Channel> findChannelByName(@Param("name") String name);
 	
-	@Query(value = "FROM channel c WHERE c.id = :id")
+	@Query(value = "FROM Channel c WHERE c.id = :id")
     Channel findAllById(@Param("id") long id);
 	
-	@Query(value="FROM channel c JOIN FETCH c.themes t WHERE t.id = :themeId")
+	@Query(value="FROM Channel c JOIN FETCH c.chanTheme t WHERE t.id = :themeId")
 	List<Channel> findByTheme(@Param("themeId") long themeId);
 }
--- SpringChat/springchat/src/main/java/org/dawan/springchat/repositories/FriendsRepository.java
+++ SpringChat/springchat/src/main/java/org/dawan/springchat/repositories/FriendsRepository.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FriendsRepository extends JpaRepository<Friends, Long> {
 
-	@Query(value = "FROM friends f WHERE f.user_id_id = :id")
+	@Query(value = "FROM Friends f WHERE f.userId = :id")
     List<Friends> findByUserId(@Param("id") long id);
 }
