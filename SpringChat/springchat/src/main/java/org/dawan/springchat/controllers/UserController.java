252be63f594c34638aae8b31a782bb4dab752ba1
kyodex <dinehs96@hotmail.fr>
2021-02-12 11:54:58 +0100

    ajout user Controller

--- SpringChat/springchat/src/main/java/org/dawan/springchat/controllers/UserController.java
+++ SpringChat/springchat/src/main/java/org/dawan/springchat/controllers/UserController.java
@@ -1,5 +1,72 @@
 package org.dawan.springchat.controllers;
 
-public class UserController {
+import java.util.List;
 
+import org.dawan.springchat.dto.MessageDto;
+import org.dawan.springchat.dto.UserDto;
+import org.dawan.springchat.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value ="/users")
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping(value="/all",produces = "application/json" )
+	public List<UserDto> getUsers(){
+		return userService.findAll();
+	}
+	
+	@GetMapping(value="/{id}",produces = "application/json" )
+	public UserDto getById(@PathVariable("id")long id) {
+		return userService.findById(id);
+	}
+	
+	@PostMapping(value="/addUser",consumes="application/json", produces = "appliction/json")
+	public UserDto addUser(@RequestBody UserDto uDto) {
+		return userService.saveOrUpdate(uDto);
+	}
+	
+	@GetMapping(value="/search",produces="application/json")
+	public List<UserDto> searchByName(@RequestParam(name = "name")String name){
+		return userService.findByName(name);
+	}
+	
+	@GetMapping(value="/search",produces="application/json")
+	public List<UserDto> searchByTheme(@RequestParam(name = "theme")String theme){
+		return userService.findByTheme(theme);
+	}
+	
+	@GetMapping(value="/search",produces="application/json")
+	public List<UserDto> searchByPlace(@RequestParam(name = "place")String place){
+		return userService.findByPlace(place);
+	}
+	
+	@DeleteMapping(value="/{id",produces="text/plain")
+	public String remove(@PathVariable("id")long id) {
+		try {
+			userService.deleteUser(id);
+			return "L'utilisateur a bien été supprimé";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Erreur : "+e.getMessage();
+		}
+	}
+	
+	@PutMapping(value="/updateUser",consumes="application/json", produces = "appliction/json")
+	public UserDto updateUser(@RequestBody UserDto uDto) {
+		return userService.saveOrUpdate(uDto);
+	}
+	
 }
